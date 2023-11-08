3e712f5654037b1e824ec568c718c41ed31277f6
Jimmy Bonilla <jimmyb091202@gmail.com>
2023-11-08 14:18:49 -0800

    a

--- workout_users/model_loader.py
+++ workout_users/model_loader.py
@@ -1,6 +1,7 @@
 
 import tensorflow as tf
+import os
 
 def load_model():
     # Load the model and return it
-    return tf.keras.models.load_model('models')
+    return tf.keras.models.load_model(os.getenv('MODEL_FILE_PATH'))
